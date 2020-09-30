6480f18bec52784b0455a2dbc51ce044ae6e5633
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2020-09-30 21:42:55 +0200

    Add threats, pageviews, uniques

--- collector.go
+++ collector.go
@@ -26,6 +26,18 @@ type CloudflareCollector struct {
 	zoneBandwidthContentType    *prometheus.Desc
 	zoneBandwidthCountry        *prometheus.Desc
 	zoneBandwidthHTTPStatus     *prometheus.Desc
+
+	// Threats
+	zoneThreatsTotal   *prometheus.Desc
+	zoneThreatsCountry *prometheus.Desc
+	zoneThreatsType    *prometheus.Desc
+
+	// Pageviews
+	zonePageviewsTotal         *prometheus.Desc
+	zonePageviewsSearchEngines *prometheus.Desc
+
+	// Uniques
+	zoneUniquesTotal *prometheus.Desc
 }
 
 func newCloudflareCollector() *CloudflareCollector {
@@ -97,6 +109,35 @@ func newCloudflareCollector() *CloudflareCollector {
 			"Bandwidth per country per status",
 			[]string{"zone", "status"}, nil,
 		),
+
+		// Threats
+		zoneThreatsTotal: prometheus.NewDesc("cloudflare_zone_threats_total",
+			"Threats per zone",
+			[]string{"zone"}, nil,
+		),
+		zoneThreatsCountry: prometheus.NewDesc("cloudflare_zone_threats_country",
+			"Threats per zone per country",
+			[]string{"zone", "country"}, nil,
+		),
+		zoneThreatsType: prometheus.NewDesc("cloudflare_zone_threats_type",
+			"Threats per zone per type",
+			[]string{"zone", "type"}, nil,
+		),
+
+		// Pageviews
+		zonePageviewsTotal: prometheus.NewDesc("cloudflare_zone_pageviews_total",
+			"Pageviews per zone",
+			[]string{"zone"}, nil,
+		),
+		zonePageviewsSearchEngines: prometheus.NewDesc("cloudflare_zone_pageviews_search_engines",
+			"Pageviews per zone per engine",
+			[]string{"zone", "searchengine"}, nil,
+		),
+		// Uniques
+		zoneUniquesTotal: prometheus.NewDesc("cloudflare_zone_uniques_total",
+			"Uniques per zone",
+			[]string{"zone"}, nil,
+		),
 	}
 }
 
@@ -119,6 +160,15 @@ func (collector *CloudflareCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.zoneBandwidthContentType
 	ch <- collector.zoneBandwidthCountry
 	ch <- collector.zoneBandwidthHTTPStatus
+	// Threats
+	ch <- collector.zoneThreatsTotal
+	ch <- collector.zoneThreatsCountry
+	ch <- collector.zoneThreatsType
+	// Pageviews
+	ch <- collector.zonePageviewsTotal
+	ch <- collector.zonePageviewsSearchEngines
+	// Uniques
+	ch <- collector.zoneUniquesTotal
 }
 
 func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
@@ -131,8 +181,7 @@ func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
 
 			zt := fetchZoneTotals(ID)
 
-			//Write latest value for each metric in the prometheus metric channel.
-			//Note that you can pass CounterValue, GaugeValue, or UntypedValue types here.
+			// Requests
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestTotal, prometheus.CounterValue, float64(zt.Requests.All), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestCached, prometheus.CounterValue, float64(zt.Requests.Cached), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestUncached, prometheus.CounterValue, float64(zt.Requests.Uncached), name)
@@ -151,11 +200,33 @@ func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
 				ch <- prometheus.MustNewConstMetric(collector.zoneRequestHTTPStatus, prometheus.CounterValue, float64(value), name, status)
 			}
 
+			// Bandwidth
 			ch <- prometheus.MustNewConstMetric(collector.zoneBandwidthTotal, prometheus.CounterValue, float64(zt.Bandwidth.All), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneBandwidthCached, prometheus.CounterValue, float64(zt.Bandwidth.Cached), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneBandwidthUncached, prometheus.CounterValue, float64(zt.Bandwidth.Uncached), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneBandwidthSSLEncrypted, prometheus.CounterValue, float64(zt.Bandwidth.SSL.Encrypted), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneBandwidthSSLUnencrypted, prometheus.CounterValue, float64(zt.Bandwidth.SSL.Unencrypted), name)
+
+			// Threats
+			ch <- prometheus.MustNewConstMetric(collector.zoneThreatsTotal, prometheus.CounterValue, float64(zt.Threats.All), name)
+
+			for country, value := range zt.Threats.Country {
+				ch <- prometheus.MustNewConstMetric(collector.zoneThreatsCountry, prometheus.CounterValue, float64(value), name, country)
+			}
+
+			for t, value := range zt.Threats.Type {
+				ch <- prometheus.MustNewConstMetric(collector.zoneThreatsType, prometheus.CounterValue, float64(value), name, t)
+			}
+
+			// Pageviews
+			ch <- prometheus.MustNewConstMetric(collector.zonePageviewsTotal, prometheus.CounterValue, float64(zt.Pageviews.All), name)
+
+			for se, value := range zt.Pageviews.SearchEngines {
+				ch <- prometheus.MustNewConstMetric(collector.zonePageviewsSearchEngines, prometheus.CounterValue, float64(value), name, se)
+			}
+
+			// Uniques
+			ch <- prometheus.MustNewConstMetric(collector.zoneUniquesTotal, prometheus.CounterValue, float64(zt.Uniques.All), name)
 
 			defer wg.Done()
 
