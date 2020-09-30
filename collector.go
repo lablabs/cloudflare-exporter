a47c2be85ea086cd7e0be8c1d2b7dfe713bfaa79
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2020-09-30 21:14:08 +0200

    Add the rest of requests metrics

--- collector.go
+++ collector.go
@@ -7,9 +7,14 @@ import (
 )
 
 type CloudflareCollector struct {
-	zoneRequestTotal    *prometheus.Desc
-	zoneRequestCached   *prometheus.Desc
-	zoneRequestUncached *prometheus.Desc
+	zoneRequestTotal          *prometheus.Desc
+	zoneRequestCached         *prometheus.Desc
+	zoneRequestUncached       *prometheus.Desc
+	zoneRequestSSLEncrypted   *prometheus.Desc
+	zoneRequestSSLUnencrypted *prometheus.Desc
+	zoneRequestContentType    *prometheus.Desc
+	zoneRequestCountry        *prometheus.Desc
+	zoneRequestHTTPStatus     *prometheus.Desc
 }
 
 func newCloudflareCollector() *CloudflareCollector {
@@ -26,6 +31,26 @@ func newCloudflareCollector() *CloudflareCollector {
 			"Number of uncached requests for zone",
 			[]string{"zone"}, nil,
 		),
+		zoneRequestSSLEncrypted: prometheus.NewDesc("cloudflare_zone_requests_ssl_encrypted",
+			"Number of encrypted requests for zone",
+			[]string{"zone"}, nil,
+		),
+		zoneRequestSSLUnencrypted: prometheus.NewDesc("cloudflare_zone_requests_ssl_unencrypted",
+			"Number of unencypted requests for zone",
+			[]string{"zone"}, nil,
+		),
+		zoneRequestContentType: prometheus.NewDesc("cloudflare_zone_requests_content_type",
+			"Number of request for zone per content type",
+			[]string{"zone", "content_type"}, nil,
+		),
+		zoneRequestCountry: prometheus.NewDesc("cloudflare_zone_requests_country",
+			"Number of request for zone per country",
+			[]string{"zone", "country"}, nil,
+		),
+		zoneRequestHTTPStatus: prometheus.NewDesc("cloudflare_zone_requests_status",
+			"Number of request for zone per HTTP status",
+			[]string{"zone", "status"}, nil,
+		),
 	}
 }
 
@@ -33,6 +58,11 @@ func (collector *CloudflareCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- collector.zoneRequestTotal
 	ch <- collector.zoneRequestCached
 	ch <- collector.zoneRequestUncached
+	ch <- collector.zoneRequestSSLEncrypted
+	ch <- collector.zoneRequestSSLUnencrypted
+	ch <- collector.zoneRequestContentType
+	ch <- collector.zoneRequestCountry
+	ch <- collector.zoneRequestHTTPStatus
 }
 
 func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
@@ -40,9 +70,6 @@ func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
 	var wg sync.WaitGroup
 	zones := fetchZones()
 	for _, z := range zones {
-		// if i == 5 {
-		// 	break
-		// }
 		wg.Add(1)
 		go func(ID string, name string) {
 
@@ -53,6 +80,21 @@ func (collector *CloudflareCollector) Collect(ch chan<- prometheus.Metric) {
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestTotal, prometheus.CounterValue, float64(zt.Requests.All), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestCached, prometheus.CounterValue, float64(zt.Requests.Cached), name)
 			ch <- prometheus.MustNewConstMetric(collector.zoneRequestUncached, prometheus.CounterValue, float64(zt.Requests.Uncached), name)
+			ch <- prometheus.MustNewConstMetric(collector.zoneRequestSSLEncrypted, prometheus.CounterValue, float64(zt.Requests.SSL.Encrypted), name)
+			ch <- prometheus.MustNewConstMetric(collector.zoneRequestSSLUnencrypted, prometheus.CounterValue, float64(zt.Requests.SSL.Unencrypted), name)
+
+			for ct, value := range zt.Requests.ContentType {
+				ch <- prometheus.MustNewConstMetric(collector.zoneRequestContentType, prometheus.CounterValue, float64(value), name, ct)
+			}
+
+			for country, value := range zt.Requests.Country {
+				ch <- prometheus.MustNewConstMetric(collector.zoneRequestCountry, prometheus.CounterValue, float64(value), name, country)
+			}
+
+			for status, value := range zt.Requests.HTTPStatus {
+				ch <- prometheus.MustNewConstMetric(collector.zoneRequestHTTPStatus, prometheus.CounterValue, float64(value), name, status)
+			}
+
 			defer wg.Done()
 
 		}(z.ID, z.Name)
